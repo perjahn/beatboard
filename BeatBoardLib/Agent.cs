8e04cf0692c76e226a2d98ef9fa3c448911b944b
Per Jahn <per.jahn@collectorbank.se>
2018-01-14 22:26:51 +0100

    Handle malformed date. Handle keyword columns.

--- BeatBoardLib/Agent.cs
+++ BeatBoardLib/Agent.cs
@@ -19,15 +19,18 @@ namespace BeatBoardLib
 
             using (HttpClient client = new HttpClient())
             {
+                int malformed = 0;
+
                 client.BaseAddress = new Uri(baseurl);
 
                 string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
 
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
+                string fieldname = "beat.name";
 
                 string url = $"{baseurl}/_search";
-                string json = "{ \"size\": 0, \"aggs\": { \"names\": { \"terms\": { \"field\": \"beat.name\", \"size\": 1000 } } } }";
+                string json = "{ \"size\": 0, \"aggs\": { \"names\": { \"terms\": { \"field\": \"" + fieldname + "\", \"size\": 1000 } } } }";
 
                 var request = new HttpRequestMessage
                 {
@@ -36,11 +39,32 @@ namespace BeatBoardLib
                     Content = new ByteArrayContent(Encoding.UTF8.GetBytes(json))
                 };
 
-
                 request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
                 var result = client.SendAsync(request).Result;
-                result.EnsureSuccessStatusCode();
+                try
+                {
+                    result.EnsureSuccessStatusCode();
+                }
+                catch (HttpRequestException)
+                {
+                    fieldname = "beat.name.keyword";
+                    Console.WriteLine($"Using fieldname: '{fieldname}'");
+                    json = "{ \"size\": 0, \"aggs\": { \"names\": { \"terms\": { \"field\": \"" + fieldname + "\", \"size\": 1000 } } } }";
+
+                    request = new HttpRequestMessage
+                    {
+                        RequestUri = new Uri(url),
+                        Method = HttpMethod.Get,
+                        Content = new ByteArrayContent(Encoding.UTF8.GetBytes(json))
+                    };
+
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    result = client.SendAsync(request).Result;
+                    result.EnsureSuccessStatusCode();
+                }
+
 
                 JObject jsonresult = JObject.Parse(result.Content.ReadAsStringAsync().Result);
 
@@ -53,7 +77,7 @@ namespace BeatBoardLib
                 {
                     string agentsname = bucket["key"].ToString();
 
-                    json = "{ \"query\": { \"term\": { \"beat.name\": \"" + agentsname + "\" } }, \"sort\": [ { \"@timestamp\": { \"order\": \"desc\", \"mode\": \"max\" } } ], \"size\": 1 }";
+                    json = "{ \"query\": { \"term\": { \"" + fieldname + "\": \"" + agentsname + "\" } }, \"sort\": [ { \"@timestamp\": { \"order\": \"desc\", \"mode\": \"max\" } } ], \"size\": 1 }";
 
                     request = new HttpRequestMessage
                     {
@@ -71,17 +95,36 @@ namespace BeatBoardLib
 
                     JObject source = (JObject)jsonresult["hits"]["hits"][0]["_source"];
 
+                    string datestring = source["@timestamp"].ToString();
+                    if (datestring.Contains(","))
+                    {
+                        malformed++;
+                        datestring = datestring.Replace(',', '.');
+                    }
+                    if (!DateTime.TryParse(datestring, out DateTime date))
+                    {
+                        Console.WriteLine($"Couldn't parse datestring: '{datestring}'");
+                        Console.WriteLine($"_id: '{jsonresult["hits"]["hits"][0]["_id"]}'");
+                        continue;
+                    }
+
+                    string version = source["beat"]["version"].ToString();
+
                     agents.Add(new Agent
                     {
                         name = agentsname,
-                        lastdate = source["@timestamp"].ToObject<DateTime>(),
-                        version = source["beat"]["version"].ToString()
+                        lastdate = date,
+                        version = version
                     });
 
                     Console.Write(".");
                 }
 
                 Console.WriteLine();
+                if (malformed > 0)
+                {
+                    Console.WriteLine($"Malformed: {malformed}");
+                }
             }
 
             return agents;
